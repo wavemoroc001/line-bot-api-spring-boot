a49e6d5d142e908e59694572eb634a63788b1fff
wavemoroc <patipan.wavemoroc@mail.kmutt.ac.th>
2021-12-23 13:31:38 +0700

    initial project

---
+++ src/test/java/com/wavemoroc/linebot/LinebotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wavemoroc.linebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinebotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
