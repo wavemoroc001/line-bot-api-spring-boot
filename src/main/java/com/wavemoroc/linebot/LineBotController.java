9c2e4f59444d1a76cc94ba06743e2225d1eab9fe
wavemoroc <patipan.wavemoroc@mail.kmutt.ac.th>
2021-12-23 15:31:08 +0700

    add echo sticker

--- src/main/java/com/wavemoroc/linebot/LineBotController.java
+++ src/main/java/com/wavemoroc/linebot/LineBotController.java
@@ -4,8 +4,10 @@ import com.linecorp.bot.client.LineMessagingClient;
 import com.linecorp.bot.model.ReplyMessage;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.StickerMessageContent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.StickerMessage;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
@@ -95,5 +97,13 @@ public class LineBotController {
         }
     }
 
+    @EventMapping
+    public void handleStickerMessage(MessageEvent<StickerMessageContent> event) {
+        log.info(event.toString());
+        StickerMessageContent message = event.getMessage();
+        reply(event.getReplyToken(), new StickerMessage(
+                message.getPackageId(), message.getStickerId()
+        ));
+    }
 
 }
